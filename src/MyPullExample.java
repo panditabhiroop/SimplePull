26ff38c5932bff9ebbf3e61a26a7072ffd3f9993
TRICONHOME\abhiroop <abhiroop@triconinfotech.com>
2016-09-15 19:50:32 +0530

    is this the reason

--- src/MyPullExample.java
+++ src/MyPullExample.java
@@ -3,7 +3,7 @@ public class MyPullExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    String[] charcters = {"Harry","Hermoine","Snipp","Ronn","Dumbeldore1"};
+    String[] charcters = {"Harry","Hermoine","Snipp","Ronn","Dumbeldore12"};
      for(int i=0; i<charcters.length;i++){
     	 System.out.println(charcters[i]);
      }
