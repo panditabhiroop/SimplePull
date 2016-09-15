7c1bd5d60039dd48b7f2dfc2e976253f1e088a33
TRICONHOME\abhiroop1 <abhiroop@triconinfotech.com>
2016-09-15 18:25:57 +0530

    Now actual change by developer2

--- src/MyPullExample.java
+++ src/MyPullExample.java
@@ -3,7 +3,7 @@ public class MyPullExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    String[] charcters = {"Harry","Hermoine","Snipp","Ronn"};
+    String[] charcters = {"Harry","Hermoine","Snipp","Ronn","Dumbeldore"};
      for(int i=0; i<charcters.length;i++){
     	 System.out.println(charcters[i]);
      }
